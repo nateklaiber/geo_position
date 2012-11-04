de528af6fed25ff1f5136e81a9c61b3b5ecf6678
Nate Klaiber <nate@theklaibers.com>
2012-11-03 19:57:07 -0700

    WIP: Some smaller helper methods as well as adding all dependencies

--- lib/geo_position.rb
+++ lib/geo_position.rb
@@ -9,9 +9,26 @@ require File.join(root, 'geo_position', 'error')
 # Conversions
 require File.join(root, 'geo_position', 'conversion')
 require File.join(root, 'geo_position', 'conversion', 'dms')
+require File.join(root, 'geo_position', 'conversion', 'latitude')
+require File.join(root, 'geo_position', 'conversion', 'longitude')
 
 # Parsers
 require File.join(root, 'geo_position', 'parser')
+require File.join(root, 'geo_position', 'parser', 'dms')
 
 module GeoPosition
+  def self.from_dms(degrees, minutes, seconds, direction)
+    conversion = GeoPosition::Conversion::Dms.new(degrees, minutes, seconds, direction)
+    conversion.to_f
+  end
+
+  def self.from_latitude(latitude)
+    conversion = GeoPosition::Conversion::Latitude.new(latitude)
+    conversion.to_s
+  end
+
+  def self.from_longitude(longitude)
+    conversion = GeoPosition::Conversion::Longitude.new(longitude)
+    conversion.to_s
+  end
 end
